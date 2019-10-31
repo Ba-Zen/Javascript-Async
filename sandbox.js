1bf708098bf3b1991261d78099802966c73b5777
Ba-Zen <bazenatlas@gmail.com>
2019-10-31 15:12:13 -0400

    http, callbacks, chaining promises

---
+++ sandbox.js
@@ -0,0 +1,60 @@
+// console.log(1);
+// console.log(2);
+
+// setTimeout(() => {
+//   console.log('callback function fired');
+// }, 2000);
+
+// console.log(3);
+// console.log(4);
+
+// Async - starts now finishes later
+
+const getTodos = resource => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting resource');
+      }
+    });
+
+    request.open('GET', resource);
+    request.send();
+  });
+};
+
+getTodos('todos/luigi.json')
+  .then(data => {
+    console.log('promise 1 resolved', data);
+    return getTodos('todos/mario.json');
+  })
+  .then(data => {
+    console.log('promise 2 resolved', data);
+    return getTodos('todos/harry.json');
+  })
+  .then(data => {
+    console.log('promise 3 resolved', data);
+  })
+  .catch(err => {
+    console.log('promise rejected', err);
+  });
+
+// console.log(1);
+// console.log(2);
+
+// getTodos((err, data) => {
+//   console.log('callback fired');
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// console.log(3);
+// console.log(4);
