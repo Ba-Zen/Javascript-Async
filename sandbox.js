d4ff62146c4023a7d2408c4a26860e77965803fa
Ba-Zen <bazenatlas@gmail.com>
2019-10-31 15:30:08 -0400

    throw and catch errors

--- sandbox.js
+++ sandbox.js
@@ -62,6 +62,10 @@
 // async & await
 const getTodos = async () => {
   const response = await fetch('todos/harry.json');
+
+  if (response.status !== 200) {
+    throw new Error('cannot fetch data');
+  }
   const data = await response.json();
 
   return data;
